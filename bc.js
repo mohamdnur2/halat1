90795e846b31a6529630ad7ae19c9421b12017a6
mohamdnur2 <44902522+mohamdnur2@users.noreply.github.com>
2018-11-15 16:11:51 +0200

    Update bc.js

--- bc.js
+++ bc.js
@@ -24,7 +24,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "508263338808180748") return;
+  if (message.author.id !== "385126161316970497") return;
 
 
   
